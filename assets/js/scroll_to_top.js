0a9f6f7064af9c751473e4d32be5878b764cd104
hritvik1 <hritvikmaini00@gmail.com>
2021-05-16 12:57:12 +0530

    final_commit

---
+++ assets/js/scroll_to_top.js
@@ -0,0 +1,10 @@
+/*   CODED BY HRITVIK MAINI   */
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+/*   CODED BY HRITVIK MAINI   */
